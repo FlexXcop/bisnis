092548d0798b498a7c49986b0b9fa9f0eb0801f8
FlexXcop <160799653+FlexXcop@users.noreply.github.com>
2024-02-22 07:12:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// toggle class active
+const navbarNav = document.querySelector
+  ('navbar-nav');
+document.querySelector('coffe-menu').
+  onlick = () => {
+    navbarNav.classList.toggle('active');
+  };
